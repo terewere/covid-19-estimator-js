822415c21f78670edd35e632a31f7809b03f4f56
shaibu zachariyya <scodegh@gmail.com>
2020-04-10 15:08:59 +0000

    setup automated testing

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,8 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => ({
+  data, // the input data you got
+  impact: {}, // your best case estimation
+  severeImpact: {} // your severe case estimation
+});
+
 
 export default covid19ImpactEstimator;
