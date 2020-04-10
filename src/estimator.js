86699ce492d140bc25639dbe858d74acc70d5080
shaibu zachariyya <scodegh@gmail.com>
2020-04-10 19:13:07 +0000

    imported and instantiated impact and severimpact classes in estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,15 @@
-const covid19ImpactEstimator = (data) => ({
-  data, // the input data you got
-  impact: {}, // your best case estimation
-  severeImpact: {} // your severe case estimation
-});
+/* eslint-disable import/extensions */
+import Impact from './impact.js';
+import SevereImpact from './severeImpact.js';
 
+const covid19ImpactEstimator = (data) => {
+  const impact = new Impact(data).getData();
+  const severeImpact = new SevereImpact(data).getData();
 
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 export default covid19ImpactEstimator;
