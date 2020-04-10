19ebcb463b0a3f4956384817ee5f1115b74e203c
shaibu zachariyya <scodegh@gmail.com>
2020-04-10 15:11:48 +0000

    added utility functions

---
+++ src/util.js
@@ -0,0 +1,31 @@
+const getFactor = ((days) => Math.floor(days / 3));
+
+const getProjectedTime = ((factor) => 2 ** factor);
+
+const convertElapseTimeTodays = ((timeToElapse, periodType) => {
+  if (periodType === 'days') {
+    return timeToElapse;
+  } if (periodType === 'weeks') {
+    return timeToElapse * 7;
+  } if (periodType === 'months') {
+    return timeToElapse * 30;
+  }
+  return 'period types include days,weeks and months';
+});
+
+const formatMoney = ((value) => {
+  // Create our number formatter.
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(value); /* $2,500.00 */
+});
+
+export {
+  getFactor,
+  getProjectedTime,
+  convertElapseTimeTodays,
+  formatMoney
+};
