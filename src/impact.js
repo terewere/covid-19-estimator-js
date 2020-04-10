87c808312a71a5265deafc932e85bab58648eafd
shaibu zachariyya <scodegh@gmail.com>
2020-04-10 19:49:11 +0000

    refactored getHospitalBedsByRequestedTime

--- src/impact.js
+++ src/impact.js
@@ -44,8 +44,7 @@ export default class Impact {
   // challenge 2 of 2
   // not sure of this computation: the question wasn't clear
   getHospitalBedsByRequestedTime() {
-    const availableBeds = 0.35 * this._input.totalHospitalBeds;
-    return Math.floor(availableBeds - this.severeCasesByRequestedTime);
+    return Math.floor(0.35 * this._input.totalHospitalBeds - this.severeCasesByRequestedTime);
   }
 
   // // challenge 3 of 1
