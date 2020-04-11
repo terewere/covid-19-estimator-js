badfd156705bb21f40ba2b3eb9253d84eac4aeaa
shaibu zachariyya <scodegh@gmail.com>
2020-04-11 17:35:03 +0000

    added xml convertor and logger middlewares

---
+++ backend/middlewares/xml-type.js
@@ -0,0 +1,13 @@
+const xmlType = (request, response, next) => {
+  request.app.isXml = false;
+
+  if (request.headers['content-type'] === 'application/xml'
+      || request.headers.accept === 'application/xml'
+      || request.params.type === 'xml'
+  ) {
+    request.app.isXml = true;
+  }
+
+  next();
+};
+export default xmlType;
