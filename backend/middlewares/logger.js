d53d599f3740c0b8b5c8364c7f9e38deb7be2499
shaibu zachariyya <scodegh@gmail.com>
2020-04-12 21:06:19 +0000

    fixing

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,19 +1,13 @@
 /* eslint-disable no-console */
-import { getActualRequestDurationInMilliseconds } from '../util';
 
 const fs = require('fs');
 
 const logger = (req, res, next) => {
   // GET /api/v1/on-covid-19 200 30 ms
-  const currentDatetime = Date.now();
-  // const { method } = req;
+  const { method } = req;
   const { url } = req;
-  // const status = res.statusCode;
-  const start = process.hrtime();
-  const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
-  const log = `${currentDatetime}\t\t${url.substring(8)}\t\tdone in ${durationInMilliseconds} seconds`;
-  // console.log(log);
-
+  const status = res.statusCode;
+  const log = `${method}\t\t${url}\t\t${status}\t\t00ms`;
   fs.appendFile('request_logs.txt', `${log}\n`, (err) => {
     if (err) {
       console.log(err);
